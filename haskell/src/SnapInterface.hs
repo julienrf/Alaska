7d612b8e862ce3b4fb057e21c91401d553375dc3
Paul Rivier <paul.r.ml@gmail.com>
2010-10-08 21:48:16 +0200

    utilisation de la monade Either pour ne pas imbriquer les case left right

--- haskell/src/SnapInterface.hs
+++ haskell/src/SnapInterface.hs
@@ -10,13 +10,24 @@ import ExprProcessor (eval, toPrefix, toInfix, toPostfix)
 
 type TransFix = Expression -> String
 
+-- | Transforme un maybe en either, utile pour insérer un message
+-- d'erreur
+maybeToEither :: e -> Maybe v -> Either e v
+maybeToEither = flip maybe Right . Left
+
+
+-- | Le getParam de base, simple Maybe, ne permet pas de mettre un
+-- message d'erreur en cas de paramètre manquant.
+getParamEither :: String -> Snap (Either String B.ByteString)
+getParamEither p = getParam (B.pack p) >>= \ mp -> 
+  return $ maybeToEither ("Missing parameter " ++ p) mp 
+
 runSnapUI :: Int -> IO ()
 runSnapUI port = do
-  putStrLn "En écoute sur http://localhost:8000"
+  putStrLn $ "En écoute sur http://localhost:" ++ (show port)
   (httpServe "*" port "Alaska Server"
    Nothing Nothing
    router)
-
 
 router, showForm, handleForm :: Snap ()
 router = (method GET showForm) <|> (method POST handleForm)
@@ -25,26 +36,23 @@ showForm = writeBS formContent
 
 -- Attention, on est dans la snap monad
 handleForm = do
-  me <- getParam "expression"
-  mf <- getParam "fixity"
-  writeBS $ case (tryComputeRes me mf) of
+  ee <- getParamEither "expression"
+  ef <- getParamEither "fixity"
+  writeBS $ case (tryComputeRes ee ef) of
     Right s -> B.pack s
     Left  e -> B.pack $ concat ["Erreur : ", show e]
 
-tryComputeRes :: Maybe B.ByteString -> Maybe B.ByteString 
+tryComputeRes :: Either String B.ByteString -> Either String B.ByteString 
                  -> Either String String
-tryComputeRes me mf = 
-  case me of
-    Nothing -> Left "Expression is missing"
-    Just e  -> case fmap fixity mf of
-      Nothing -> Left "Fixity is missing"
-      Just mf' -> case mf' of
-        Nothing -> Left "Wrong fixity"
-        Just f  -> case parseExpr (B.unpack e) of
-          Left err -> Left $ concat ["Parse Error : ", show err]
-          Right pe -> Right $ concat ["L'expression est : ",f pe,"\n",
-                                      "Sa valeur est : ", show $ eval pe]
-        
+tryComputeRes ee esf = do
+  e <- ee
+  sf <- esf
+  f <- maybeToEither "Fixity is invalid" $ fixity sf
+  case parseExpr (B.unpack e) of
+    Left err -> Left $ concat ["Parse Error : ", show err]
+    Right pe -> Right $ concat ["L'expression est : ",f pe,"\n",
+                                "Sa valeur est : ", show $ eval pe]
+
 fixity :: B.ByteString -> Maybe TransFix
 fixity f =  lookup f [("prefix", toPrefix),
                       ("infix", toInfix),
@@ -68,3 +76,4 @@ formContent = "<HTML>\
 \    </FORM>\
 \  </BODY>\
 \</HTML>"
+
